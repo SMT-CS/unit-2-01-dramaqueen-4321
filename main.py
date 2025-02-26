d7b07517595ac57f0ec373f742dabd8a2a96695a
dramaqueen-4321 <blessing.abolarin@stu.ocsb.ca>
2025-02-26 02:18:19 +0000

    some commit message

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Created by: Blessing Abolarin
+Created on: Feb 2025
+This module calculates the arcircumfrence of a circle
+    with radius 21 mm.
+"""
+
+import math
+
+
+def main() -> None:
+    """
+    The main() function calculates the Circomfrence of a circle with a diameter of 42,
+        returns None.
+    """
+    print("If a circle has a radius of 21 mm:")
+    print("")
+    print(f"The circumfrence is {(2 * math.pi * 21):.2f} mm².")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+
+    main()
